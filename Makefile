b3a6edb69ecff2c6c0d9305a50c8ec0a9077024a
Rich Tong <1782087+richtong@users.noreply.github.com>
2022-06-17 11:50:55 -0700

    fix: Makefile lint, remove checkmake
    Alphabetize gitattributes.base
    Remove checkmake from pre-commit-config.full does not work
    Makefile complies with checkmake

--- Makefile
+++ Makefile
@@ -2,4 +2,19 @@
 #
 # Release tag
 TAG=0.9
+
+## test: test the library
+.PHONY: test
+test:
+	@echo "insert test code here..."
+
+## clean: remove the build directory
+.PHONY: clean
+clean:
+	@echo "insert clear code here..."
+
+## all: build all
+.PHONY: all
+
+
 include include.mk
--- include.mk
+++ include.mk
@@ -27,6 +27,11 @@ RUN ?=
 help: $(MAKEFILE_LIST)
 	@sed -n 's/^##//p' $(MAKEFILE_LIST)
 
+## These are required tags from checkmate stubs are here you should overwrite
+.PHONY: test
+test:
+	@echo "test stub"
+
 ## tag: pushes a new tag up while delete old to force the action
 .PHONY: tag
 tag:
