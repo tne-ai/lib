30e2d186b1c9a20ed3d7a28a37b8b9d1c7296548
Rich <rich@tongfamily.com>
2022-12-22 23:59:01 +0000

    fix: avahi works for ssh and sftp-ssh
    The two services need -f because device information only works for
    service like _nfs._tcp or _smb._tcp
    
    Also more sophisticated grep of the /var/log/syslog

--- lib-avahi.sh
+++ lib-avahi.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+## vim: se noet ts=4 sw=4:
 ##
 ## Avahi publishing
 ## Either can do static publish via a file
@@ -6,17 +7,31 @@
 ## Use the later when the service is only up temporarily
 ##
 
-# avahi_publish service hostname protocol port [text]
+# avahi_publish -f service hostname protocol port [text]
+# -d means add device info
 # Adds an nice Mac icon as well
 avahi_publish() {
 	if [[ ! $OSTYPE =~ linux ]]; then
 		log_verbose for linux only
 		return
 	fi
+	local DEVICE_INFO=false
+	while [[ $1 =~ ^- ]]; do
+		log_verbose "found flag $1"
+		if [[ $1 == -f ]]; then
+			log_verbose "Set DEVICE_INFO"
+			local DEVICE_INFO=true
+			shift
+		fi
+		flags+=("$1")
+		shift
+	done
+	log_verbose "DEVICE_INFO=$DEVICE_INFO"
 	# http://www.win.tue.nl/~johanl/educ/IoT-Course/mDNS-SD%20Tutorial.pdf
 	# note this file must end in .service
 	local dir="/etc/avahi/services"
-	local service_file="$dir/${1:-nfs}.service"
+	local service="${1:-nfs}"
+	local service_file="$dir/$service.service"
 	local name="${2:-"$HOSTNAME nfs"}"
 	local protocol="${3:-"_nfs._tcp"}"
 	local port="${4:-2049}"
@@ -27,35 +42,48 @@ avahi_publish() {
 		return
 	fi
 
-	log_verbose "Ubuntu 14.04 does not like this header and throws an error"
+	#log_verbose "Ubuntu 14.04 does not like this header and throws an error"
+	#<?xml version="1.0" standalone='no'?><!--*-nxml-*-->
+	#<!DOCTYPE service=group SYSTEM "avahi-service.dtd">
+	# make sure you are using tabs as the xml must not be indented
+	log_verbose "add service group to $service_file"
 	sudo tee "$service_file" <<-EOF
-		        <?xml version="1.0" standalone='no'?><!--*-nxml-*-->
-		        <!DOCTYPE service=group SYSTEM "avahi-service.dtd">
-				<service-group>
-				  <name replace-wildcards="yes">$name</name>
-				  <service>
-				     <type>$protocol</type>
-				     <port>$port</port>
+		<service-group>
+		  <name replace-wildcards="yes">$name</name>
+		  <service>
+		     <type>$protocol</type>
+		     <port>$port</port>
 	EOF
-	# optionally put in  a text record
 	if [[ -n $text ]]; then
+		log_verbose "optionally put in a text description"
 		sudo tee -a "$service_file" <<<"    <txt-record>$text</txt-record>"
 	fi
-	# http://simonwheatley.co.uk/2008/04/avahi-finder-icons/
+	log_verbose "end the service"
 	sudo tee -a "$service_file" <<-EOF
 		  </service>
-		  <service>
-		       <type>_device-info._tcp</type>
-		       <port>0</port>
-		       <txt-record>model=RackMac</txt-record>
-		   </service>
+	EOF
+	# http://simonwheatley.co.uk/2008/04/avahi-finder-icons/
+	if $DEVICE_INFO; then
+		log_verbose "Put optional device info in the finder icon"
+		sudo tee -a "$service_file" <<-EOF
+			  <service>
+			       <type>_device-info._tcp</type>
+			       <port>0</port>
+			       <txt-record>model=RackMac</txt-record>
+			   </service>
+		EOF
+	fi
+	sudo tee -a "$service_file" >/dev/null <<-EOF
 		</service-group>
 	EOF
 
-	log_verbose "added avahi $name and check now for success in log"
 	# with Ubuntu 16.04 the name comes out as capital letters use -i
 	# http://droptips.com/using-grep-and-ignoring-case-case-insensitive-grep
-	if ! sudo grep -qi "$name" /var/log/syslog; then
+	# need -a since /var/log/syslog has some binary characters in it
+	# https://unix.stackexchange.com/questions/335716/grep-returns-binary-file-standard-input-matches-when-trying-to-find-a-string
+	# we do not search for $name because it may be %h for the hostname
+	if ! sudo grep -qa "avahi-daemon.*/services/$service.service.*successfully established" /var/log/syslog; then
+		log_verbose "publishing $service did not appear in /var/log/syslog"
 		return 1
 	fi
 
