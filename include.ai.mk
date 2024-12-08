34df81de19c04e90fa8e4117a19d992a3c551510
Rich Tong <1782087+richtong@users.noreply.github.com>
2024-12-08 15:21:44 -0800

    feat: ngrok frontend open-webui
    Adding ngrok as the frontend for openwebui to provide ssl protection and
    oauth

--- include.ai.mk
+++ include.ai.mk
@@ -3,42 +3,48 @@
 #
 FLAGS ?=
 SHELL := /usr/bin/env bash
-# port does not work use 8080 default
+# port does not work use 8080 default and is deprecated
 # PORT ?= 1314
-PORT ?= 8080
 # does not work the EXCLUDEd directories are still listed
 # https://www.theunixschool.com/2012/07/find-command-15-examples-to-EXCLUDE.html
 # EXCLUDE := -type d \( -name extern -o -name .git \) -prune -o
 # https://stackoverflow.com/questions/4210042/how-to-EXCLUDE-a-directory-in-find-command
 #
 # https://www.oreilly.com/library/view/managing-projects-with/0596006101/ch04.html
+PORT ?= 8080
 define START_SERVER
-	if (($$(pfind $(1) | wc -l) == 0)); then $(1) serve $(2) $(3) $(4) $(5); fi &
+if (($$(pfind $(1) | wc -l) == 0)); then $(1) $(2) $(3) $(4) $(5) $(6) $(7) $(8) $(9); fi &
 endef
 
 ## ai: start all ai servers
 .PHONY: ai
 ai: ollama open-webui
 
-## open-webui: run open webui as frontend to ollama at port 1314 change with PORT=port
+## open-webui: run open webui as frontend which is hard coded at 8080
 .PHONY: open-webui
-open-webui: open-webui.kill
-	$(call START_SERVER,open-webui,--port $(PORT))
+open-webui:
+	$(call START_SERVER,open-webui,serve)
 
 ## ollama: run ollama at http://localhost:11434 change with OLLAMA_HOST=127.0.0.1:port
 .PHONY: ollama
-ollama: ollama.kill
-	$(call START_SERVER,ollama)
+ollama:
+	$(call START_SERVER,ollama,serve)
 
 ## ai.kill: kill all ai all ai servers
 .PHONY: ai.kill
-ai.kill: ollama.kill open-webui.kill
-
+ai.kill: ollama.kill open-webui.kill ngrok.kill
 
 ## ollama.kill: kill the ollama server
 ## open-webui.kill: kill the open webui server
-# ignore with a dash in gnu make
+# ignore with a dash in gnu make so || true isn't needed but there in case
 # https://www.gnu.org/software/make/manual/make.html#Errors
 # -f means find anywhere in the argument field
 %.kill:
 	-pkill -f "$*" || true
+
+## ngrok: authentication front-end for open-webui uses 1Password to 8080
+# doing a pkill before seems to stop the run so only ai.kill does the stopping
+.PHONY: ngrok
+ngrok:
+	$(call START_SERVER,ngrok,http,--url="$$(op item get 'ngrok' --field 'static domain')","$(PORT)", \
+		--oauth=google,--oauth-allow-domain=tne.ai, --oauth-allow-domain=tongfamily.com)
