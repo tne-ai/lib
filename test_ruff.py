d8735c4eacdec59efe12fee004a772935402bc3e
Rich Tong <1782087+richtong@users.noreply.github.com>
2024-12-07 23:59:43 -0800

    fix: precommit works
    pre-commit cli2 needs a pinned node version

--- test_ruff.py
+++ test_ruff.py
@@ -4,16 +4,16 @@ from pathlib import Path
 
 
 def add(a: int, b: int) -> int:
-  """Add two numbers."""
-  return a + b
+    """Add two numbers."""
+    return a + b
 
 
 def main() -> None:
-  """Find out where you are."""
-  print("Hello, World!")
-  print(Path.cwd())
-  print(add(1, 2))
+    """Find out where you are."""
+    print("Hello, World!")
+    print(Path.cwd())
+    print(add(1, 2))
 
 
 if __name__ == "__main__":
-  main()
+    main()
