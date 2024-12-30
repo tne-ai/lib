e3fff1c404f82f1291dbdd1e7409530a433a0e81
Rich Tong <1782087+richtong@users.noreply.github.com>
2024-12-30 12:37:47 -0800

    fix: make ai to pgrep
    pfind was not working but pgrep is per
    Adding tika for open-webui

--- include.ai.mk
+++ include.ai.mk
@@ -13,28 +13,44 @@ SHELL := /usr/bin/env bash
 # https://www.oreilly.com/library/view/managing-projects-with/0596006101/ch04.html
 PORT ?= 8080
 define START_SERVER
-if (($$(pfind $(1) | wc -l) == 0)); then $(1) $(2) $(3) $(4) $(5) $(6) $(7) $(8) $(9); fi &
+if ! pgrep -L $(1) ; then echo start $(2) && $(2) $(3) $(4) $(5) $(6) $(7) $(8) $(9) $(10); fi &
 endef
 
 ## ai: start all ai servers
 .PHONY: ai
-ai: ollama open-webui
+ai: ollama tika open-webui
 
 ## open-webui: run open webui as frontend which is hard coded at 8080
 .PHONY: open-webui
-open-webui: open-webui.kill
-	$(call START_SERVER,open-webui,serve)
+open-webui:
+	$(call START_SERVER,open-webui,open-webui,serve)
 
 ## ollama: run ollama at http://localhost:11434 change with OLLAMA_HOST=127.0.0.1:port
 # https://docs.openwebui.com/troubleshooting/connection-error/
 # 0.0.0.0 means it will serve remote openwebui clients
 .PHONY: ollama
 ollama:
-	OLLAMA_HOST=0.0.0.0 $(call START_SERVER,ollama,serve)
+	export OLLAMA_HOST=0.0.0.0 && $(call START_SERVER,ollama,ollama,serve)
+
+## ngrok: authentication front-end for open-webui uses 1Password to 8080 onlyi 1 active at a time
+# doing a pkill before seems to stop the run so only ai.kill does the stopping
+.PHONY: ngrok
+ngrok:
+	$(call START_SERVER,ngrok,ngrok,http,--url="$$(op item get 'ngrok' --field 'static domain')","$(PORT)", \
+		--oauth=google,--oauth-allow-domain=tne.ai, --oauth-allow-domain=tongfamily.com)
+
+
+TIKA_VERSION ?= 2.9.2
+TIKA_JAR ?= tika-server-standard-$(TIKA_VERSION).jar
+
+## tika: run the tika server at 9998
+.PHONY: tika
+tika:
+	$(call START_SERVER,$(TIKA_JAR),java,-jar,"$$HOME/jar/$(TIKA_JAR)")
 
 ## ai.kill: kill all ai all ai servers
 .PHONY: ai.kill
-ai.kill: ollama.kill open-webui.kill ngrok.kill
+ai.kill: ollama.kill open-webui.kill ngrok.kill tika.kill
 
 ## %.kill:
 # ignore with a dash in gnu make so || true isn't needed but there in case
